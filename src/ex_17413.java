2e07b9eabe04b9625979eb50b6885ecd8f733f8c
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-02-28 20:42:42 +0900

    WordReverse

---
+++ src/ex_17413.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Stack;
+
+public class ex_17413 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String sentence = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        boolean tag = false;
+
+        for (char c : sentence.toCharArray()) {
+            if (c == ' ' || c == '<') {
+                if (c == '<'){
+                    tag = true;
+                }
+
+                while(!stack.empty()) {
+                    sb.append(stack.pop());
+                }
+
+                sb.append(c);
+            } else if (tag) {
+                sb.append(c);
+
+                if (c == '>') {
+                    tag = false;
+                }
+            }
+            else {
+                stack.push(c);
+            }
+        }
+
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb);
+    }
+}
