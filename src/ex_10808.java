2f1630b2c14789b952b613cf306ee7dbe4fde618
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-03-06 16:02:27 +0900

    WordCount

---
+++ src/ex_10808.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class ex_10808 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        int[] cntArr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            cntArr[(int)s.charAt(i) - 97]++;
+        }
+
+        for (int i = 0; i < cntArr.length; i++) {
+            sb.append(cntArr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
