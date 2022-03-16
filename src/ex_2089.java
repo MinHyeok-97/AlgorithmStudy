27fe7f512386e19168400cc4f3e75d89daa8186d
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-03-16 14:01:01 +0900

    -2진수

---
+++ src/ex_2089.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class ex_2089 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        if (num == 0) {
+            System.out.println(0);
+        } else {
+            while (num != 1) {
+                sb.append(Math.abs(num % -2));
+                num = (int)Math.ceil((double) num / -2);
+            }
+
+            sb.append(num);
+
+            System.out.println(sb.reverse());
+        }
+    }
+}
