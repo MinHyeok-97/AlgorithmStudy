8f963bc720584bd65253274a1863f10e3bc4ce47
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-03-28 23:47:28 +0900

    대소문자 변환

---
+++ src/inflearn/string/ex_2.java
@@ -0,0 +1,28 @@
+package inflearn.string;
+
+import java.io.*;
+
+public class ex_2 {
+    static String solution(String str) {
+        String answer = "";
+
+        for (char ch : str.toCharArray()){
+//            if(ch >= 'A' && ch <= 'Z')
+            if (Character.isUpperCase(ch)) {
+                answer += Character.toLowerCase(ch);
+            } else {
+                answer += Character.toUpperCase(ch);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+}
