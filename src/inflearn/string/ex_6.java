9d74705e0dff20944382f1ef2a750243bf283f9a
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-03-29 21:11:17 +0900

    중복 문자 제거

---
+++ src/inflearn/string/ex_6.java
@@ -0,0 +1,24 @@
+package inflearn.string;
+
+import java.io.*;
+
+public class ex_6 {
+    static String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+}
