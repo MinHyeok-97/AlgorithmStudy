52ce70d2f83df3d794e3365a6249bb20ef6e393e
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-05-30 17:18:25 +0900

    Queue

---
+++ src/inflearn/stacknqueue/ex_7.java
@@ -0,0 +1,35 @@
+package inflearn.stacknqueue;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//교육과정 설계
+public class ex_7 {
+    static String solution(String need, String plan) {
+        String answer = "YES";
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : need.toCharArray()) {
+            queue.offer(c);
+        }
+
+        for (char c : plan.toCharArray()) {
+            if (queue.contains(c)) {
+                if (c != queue.poll()) {
+                    return "NO";
+                }
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        System.out.println(solution(a, b));
+    }
+}
