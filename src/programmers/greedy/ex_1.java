34fe8b7f99c8eed41132e8151049048596652cc1
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-08-11 16:32:41 +0900

    greedy - 체육복

--- src/programmers/greedy/ex_1.java
+++ src/programmers/greedy/ex_1.java
@@ -4,6 +4,34 @@ import java.util.Arrays;
 
 //체육복
 public class ex_1 {
+    static int solution2(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        int[] student = new int[n];
+
+        for (int l : lost) {
+            student[l - 1]--;
+        }
+        for (int r : reserve) {
+            student[r - 1]++;
+        }
+
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] == -1) {
+                if (i - 1 >= 0 && student[i - 1] == 1) {
+                    student[i]++;
+                    student[i - 1]--;
+                } else if (i + 1 < student.length && student[i + 1] == 1) {
+                    student[i]++;
+                    student[i + 1]--;
+                } else {
+                    answer--;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     static int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
 
