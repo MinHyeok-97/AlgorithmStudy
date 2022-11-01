e607266f6387ddc2f0daa88913e5da4c43df8846
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-11-01 14:29:44 +0900

    프로그래머스 - 피보나치 수

---
+++ src/programmers/date/D20221101a.java
@@ -0,0 +1,26 @@
+package programmers.date;
+
+import java.io.*;
+
+//피보나치 수
+public class D20221101a {
+    static int[] f;
+    static int solution(int n) {
+        f = new int[n + 1];
+
+        return dfs(n);
+    }
+
+    static int dfs(int n) {
+        if (n == 1 || n == 2) return f[n] = 1;
+        else if (f[n] > 0) return f[n];
+
+        return f[n] = (dfs(n - 1) + dfs(n - 2)) % 1234567;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(solution(Integer.parseInt(br.readLine())));
+    }
+}
