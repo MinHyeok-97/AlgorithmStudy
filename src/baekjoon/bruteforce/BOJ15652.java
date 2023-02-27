74443f2564cdf60f0434dc88167b93025b9d3198
Minhyeok Kang <kmhyeok0816@gmail.com>
2023-02-27 23:27:05 +0900

    완전탐색 - N과M(4)

---
+++ src/baekjoon/bruteforce/BOJ15652.java
@@ -0,0 +1,54 @@
+package baekjoon.bruteforce;
+
+/*
+N과 M (4)
+문제
+    자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+        1부터 N까지 자연수 중에서 M개를 고른 수열
+        같은 수를 여러 번 골라도 된다.
+        고른 수열은 비내림차순이어야 한다.
+            길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+입력
+    첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+    한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+    수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ15652 {
+    static int n, m;
+    static int[] select;
+    static StringBuilder sb = new StringBuilder();
+
+    static void rec_func(int k, int start) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(select[i]).append(' ');
+            }
+            sb.append("\n");
+        } else {
+            for (int i = start; i <= n; i++) {
+                select[k] = i;
+                rec_func(k + 1, i);
+                select[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        select = new int[m];
+
+        rec_func(0, 1);
+
+        System.out.println(sb);
+    }
+}
